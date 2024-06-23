8defdac3e705ae011bff953da9240687a5bd1e07
Jabril Mahamud (personal account) <59336921+Jabril-Mahamud@users.noreply.github.com>
2024-06-23 13:24:27 +0100

    moved the old blog to comminuty page

--- app/routes/blog.tsx
+++ app/routes/blog.tsx
@@ -1,7 +1,9 @@
-import History from "@/components/history"
+import CommunityTable from "@/components/history"
 
 export default function Blog() {
 	return (
-        <History/>
+        <div className="flex justify-center">
+            <h1>Blog History</h1>
+        </div>
     )
 }
--- app/routes/community.tsx
+++ app/routes/community.tsx
@@ -1,8 +1,10 @@
+import CommunityTable from "@/components/history";
 
 export default function Community() {
 	return (
         <div className="flex justify-center">
             <h1>Community</h1>
+            <CommunityTable/>
         </div>
     )
 }
